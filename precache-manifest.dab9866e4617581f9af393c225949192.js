16c8229b21968fb5cb1b7185ce2eabf6248f94af
Code Sandbox <hello@codesandbox.io>
2021-08-19 18:35:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dab9866e4617581f9af393c225949192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd1ef55a0f9ba3bdeff0f0a1f7922980",
+    "url": "https://tapudp.github.io/csb-6k0ur/index.html"
+  },
+  {
+    "revision": "d0e737307ff05884f8f5",
+    "url": "https://tapudp.github.io/csb-6k0ur/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "22ff18b3466807051480",
+    "url": "https://tapudp.github.io/csb-6k0ur/static/js/2.71305984.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://tapudp.github.io/csb-6k0ur/static/js/2.71305984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e737307ff05884f8f5",
+    "url": "https://tapudp.github.io/csb-6k0ur/static/js/main.5f0f8470.chunk.js"
+  },
+  {
+    "revision": "9e35d03ce74e8f48b04c",
+    "url": "https://tapudp.github.io/csb-6k0ur/static/js/runtime-main.e3e816ba.js"
+  }
+]);
